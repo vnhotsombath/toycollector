a350a6163f17078c7cc952b7a8a75b6e7fafb4dd
username <youremail@domain.com>
2022-08-29 14:25:54 -0400

    migrate toy model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Toy(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
