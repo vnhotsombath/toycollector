1f439c543fafdbd01177dda45c900baba3b44871
username <youremail@domain.com>
2022-08-30 21:22:03 -0400

    details

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Toy
