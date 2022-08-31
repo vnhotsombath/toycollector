c76e260ecad79b47cb3a3efa5bbdf7512a549272
username <youremail@domain.com>
2022-08-31 17:31:36 -0400

    add Piece model

--- main_app/models.py
+++ main_app/models.py
@@ -10,6 +10,20 @@ class Toy(models.Model):
     def get_absolute_url(self):
         return reverse('detail', kwargs={'toy_id': self.id})
 
+PIECES = (
+    ('W', 'Weapon'),
+    ('I', 'Interchangeable body piece'),
+    ('B', 'Bonus piece to build a special character')
+)
+
+class Piece(models.Model):
+    piece = models.CharField(
+        max_length=10,
+        choices = PIECES,
+        default = PIECES [0][0]
+        )
+    toy = models.ForeignKey(Toy, on_delete = models.CASCADE)
+
 class Ability(models.Model):
     description = models.CharField(max_length=100)    
 
