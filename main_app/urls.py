c165693c562f7248766044e830ac4376b1eb64d0
username <youremail@domain.com>
2022-08-31 20:20:31 -0400

    ability form list detail delete

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,4 +10,9 @@ urlpatterns = [
     path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'),
     path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
     path('toys/<int:toy_id>/add_piece/', views.add_piece, name='add_piece'),
+    path('abilities/', views.AbilityList.as_view(), name='abilities_index'),
+    path('abilities/<int:pk>/', views.AbilityDetail.as_view(), name='abilities_detail'),
+    path('abilities/create/', views.AbilityCreate.as_view(), name='abilities_create'),
+    path('abilities/<int:pk>/upate/', views.AbilityUpdate.as_view(), name='abilities_update'),
+    path('abilities/<int:pk>/delete/', views.AbilityDelete.as_view(), name='abilities_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Toy
+from django.views.generic import ListView, DetailView
+from .models import Toy, Ability
 from .forms import PieceForm, AbilityForm
 
 # define the home view
 def home(request):
-    return HttpResponse('<h1>Welcome to the Trifecta Toy Collector Page!</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
@@ -18,8 +19,8 @@ def toys_index(request):
 def toys_detail(request, toy_id):
     toy = Toy.objects.get(id=toy_id)
     piece_form = PieceForm()
-    ability_form = AbilityForm()
-    return render(request, 'toys/detail.html', { 'toy': toy, 'piece_form': piece_form, 'ability_form': ability_form })
+  
+    return render(request, 'toys/detail.html', { 'toy': toy, 'piece_form': piece_form })
 
 def add_piece(request, toy_id):
     form = PieceForm(request.POST)
@@ -36,6 +37,24 @@ def add_ability(request, toy_id):
         new_ability = toy_id = toy_id
         new_ability.save()
     return redirect('detail', toy_id=toy_id)
+
+class AbilityList(ListView):
+    model = Ability
+
+class AbilityDetail(DetailView):
+    model = Ability 
+
+class AbilityCreate(CreateView):
+    model = Ability
+    fields = '__all__'
+
+class AbilityUpdate(UpdateView):
+    model = Ability
+    fields = ['description']
+
+class AbilityDelete(DeleteView):
+    model = Ability
+    success_url = '/toys/'
 
 class ToyCreate(CreateView):
     model = Toy
